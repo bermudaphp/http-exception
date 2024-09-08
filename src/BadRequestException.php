c98c35ae2d5f36cd5d0c1cb75cc8adeca7f4714a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-09-08 16:47:19 +0200

    Update BadRequestException.php

--- src/BadRequestException.php
+++ src/BadRequestException.php
@@ -6,6 +6,6 @@ class BadRequestException extends HttpException
 {
     public function __construct(string $reasonPhrase = null)
     {
-        parent::__construct($reasonePhrase, 400);
+        parent::__construct(400, $reasonPhrase);
     }
 }
